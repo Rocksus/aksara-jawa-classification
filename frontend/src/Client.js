72ae41db74209a04779056248c5933b322c55853
Ray Antonius <rayantonius@gmail.com>
2022-01-05 00:37:21 +0700

    change target address

--- frontend/src/Client.js
+++ frontend/src/Client.js
@@ -4,7 +4,7 @@ async function sendData(canvas, ref) {
     const file = canvas.toDataURL("image/png");
     let data = new FormData();
     data.append('img', file)
-    axios.post(`http://34.101.94.47:14022/classify`, data)
+    axios.post(`http://localhost:14022/classify`, data)
         .then(function (response) {
             ref.changeState(response.data.classID, response.data.name)
         })
